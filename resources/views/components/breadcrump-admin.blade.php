3b659b525237468d0b359bbf272113e46bdf861a
Nabil Ghulam <nabilghlm12@gmail.com>
2025-10-18 09:51:31 +0700

    twenty edit

--- resources/views/components/breadcrump-admin.blade.php
+++ resources/views/components/breadcrump-admin.blade.php
@@ -4,7 +4,6 @@
     @foreach ($items as $index => $crumb)
         @if (!empty($crumb['url']) && $index !== array_key_last($items))
             <a href="{{ $crumb['url'] }}">{{ $crumb['label'] }}</a>
-            <span class="separator">/</span>
         @else
             <span class="current">{{ $crumb['label'] }}</span>
         @endif
